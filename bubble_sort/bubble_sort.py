70672bf41da1302e9e946bcac30aebcab6a8997c
Devyan Jethwa A <vrindevj@gmail.com>
2025-06-20 11:40:53 +0300

    Changes made to bubble sort and selection sort.

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,17 @@
 def bubble_sort(unsorted_list):
     # TODO: Implement bubble sort
-    pass
+    """Sorts a list using bubble sort on a linked list with length x."""
+
+    x = len(unsorted_list)
+
+    for f in range(x): 
+        swapped = False  #used to track if any swaps occur. if no swaps occur    this means the list has been sorted.
+
+        for j in range(0, x-f-1): # range given ensure that we dont compare elements that we have already sorted.
+            if unsorted_list[j] > unsorted_list[j+1]: 
+                unsorted_list[j], unsorted_list[j+1] = unsorted_list[j+1], unsorted_list[j] # the   unsorted_list[j] loop reiterates the through the unsorted portion of the list comparing it to the next element unsorted_list[j+1]
+                swapped = True
+        if not swapped:
+            break
+    return unsorted_list
+   # pass
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,21 @@
-def selection_sort(arr):
+def selection_sort(unsorted_list):
+
     # TODO: Implement selection sort
-    pass
+    x = len(unsorted_list) # to fing the length of the unsorted list
+
+    for j in range(x):
+        min_index = j #this is done to find the minimum element in the unsorted part of the list is the first element
+       
+        for k in range(j+1, x):
+            if unsorted_list[k] < unsorted_list[min_index]:
+                min_index = k   
+
+
+        # Swap the found minimum element with the first element of the unsorted part and put it in the sorted part
+        unsorted_list[j], unsorted_list[min_index] = unsorted_list[min_index], unsorted_list[j]
+
+    
+    return unsorted_list  
+
+
+
